4a87c3f8a9a593edde3d18cadb6d73c9d3eaf349
Ferseo <90406074+Ferseo@users.noreply.github.com>
2022-07-25 19:43:12 +0200

    optimized routes
    En este paso se eliminan las rutas de las tablas antiguas y se crea la de la tabla de materiales global

--- gestinventario_app/routes/api.php
+++ gestinventario_app/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AtrezzoController;
 use App\Http\Controllers\CableadoController;
 use App\Http\Controllers\CredencialesController;
 use App\Http\Controllers\IluminacionController;
+use App\Http\Controllers\MaterialController;
 use App\Http\Controllers\MaterialmontajeController;
 use App\Http\Controllers\MaterialprestadoController;
 use App\Http\Controllers\OtrosController;
@@ -28,29 +29,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::controller(AtrezzoController::class)->group(function () {
-    Route::get('/atrezzo', 'getAll');
-    Route::post('/atrezzo/store', 'store');
-    Route::put('/atrezzo/edit', 'edit');
+
+Route::controller(MaterialController::class)->group(function () {
+    Route::get('/material', 'getAll');   
+    Route::post('/material/store', 'store'); 
+    Route::put('/material/edit', 'edit');    
 });
 
-Route::controller(CableadoController::class)->group(function () {
-    Route::get('/cableado', 'getAll');
-    Route::post('/cableado/store', 'store');
-    Route::put('/cableado/edit', 'edit');
-});
-
-Route::controller(IluminacionController::class)->group(function () {
-    Route::get('/iluminacion', 'getAll');
-    Route::post('/iluminacion/store', 'store');
-    Route::put('/iluminacion/edit', 'edit');   
-});
-
-Route::controller(MaterialmontajeController::class)->group(function () {
-    Route::get('/materialmontaje', 'getAll');   
-    Route::post('/materialmontaje/store', 'store'); 
-    Route::put('/materialmontaje/edit', 'edit');    
-});
 
 Route::controller(MaterialprestadoController::class)->group(function () {
     Route::get('/materialprestado', 'getAll');
@@ -58,28 +43,11 @@ Route::controller(MaterialprestadoController::class)->group(function () {
     Route::put('/materialprestado/edit', 'edit');   
 });
 
-Route::controller(OtrosController::class)->group(function () {
-    Route::get('/otros', 'getAll');
-    Route::post('/otros/store', 'store'); 
-    Route::put('/otros/edit', 'edit');   
-});
-
-Route::controller(SonidoController::class)->group(function () {
-    Route::get('/sonido', 'getAll'); 
-    Route::post('/sonido/store', 'store');  
-    Route::put('/sonido/edit', 'edit'); 
-});
 
 Route::controller(TareasController::class)->group(function () {
     Route::get('/tareas', 'getAll');
     Route::post('/tareas/store', 'store');
     Route::put('/tareas/edit', 'edit');    
-});
-
-Route::controller(VideoController::class)->group(function () {
-    Route::get('/video', 'getAll');  
-    Route::post('/video/store', 'store');
-    Route::put('/video/edit', 'edit');  
 });
 
 Route::controller(CredencialesController::class)->group(function () {
